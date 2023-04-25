76c75ce637359ebdbcbd54fea550285942356ef9
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-04-25 10:23:24 -0300

    Update urls.py

--- investimento/urls.py
+++ investimento/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'investimento'
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('', views.HomeView.as_view(), name='home'),
+    path('<int:pk>/', views.CalcTot.as_view(), name='detail'),
 ]
