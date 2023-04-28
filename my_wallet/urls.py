844c4c30bac683b0a86e9015ba6d665011311e36
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-04-28 18:27:40 -0300

    Create urls.py

---
+++ my_wallet/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'my_wallet'
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='transaction'),
+]
