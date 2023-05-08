99021131ebe6a586347c4b9e38ef60006418b42a
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-05-08 16:21:17 -0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-
+from django.utils import timezone
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
